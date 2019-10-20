dc490cf07a92a1cfd48484139988f1af6cfd335c
joaopedrobritot <47987224+joaopedrobritot@users.noreply.github.com>
2019-10-20 20:01:17 -0300

    Add files via upload

--- Lista 1/L1Q6.asm
+++ Lista 1/L1Q6.asm
@@ -12,54 +12,61 @@
 #	}
 #	return Fibonacci(n - 1) + Fibonacci(n - 2);
 #}
+Main:
+	addi $s0, $zero, 6 # n = 6 /// também servirá de indice para a recursao
 
-# Falta explicar
+	addi $s4, $zero, 0 # Resultado /// Fibonacci(n) == F(n)
 
-addi $s0, $zero, 6 # Fibonacci(n) == Fn
+	jal Fibonacci # chamada da funcao
 
-addi $s2, $zero, 0 # Fn - 1
-addi $s3, $zero, 0 # Fn - 2
-
-addi $s4, $zero, 0 # Resultado
-
-jal Fibonacci
-
-j EXIT
+	j EXIT
 
 Fibonacci:
 	addi $sp, $sp, -4
-	sw $ra, 0($sp)
+	sw $ra, 0($sp) # guardo o endereço de retorno da chamada recursiva
 	
-	slti $t0, $s0, 1
-	bne $t0, $zero, return1
-	slti $t0, $s0, 3
-	bne $t0, $zero, return2
+	slti $t0, $s0, 1 # n < 1 ?
+	bne $t0, $zero, return0 # se sim entao retorno 0, senao verifico a prox condicao
+	slti $t0, $s0, 3 # já que n >= 1, então verifico só se ele n < 3 ?
+	bne $t0, $zero, return1 # se sim entao retorne 1, senao:
 	
 	addi $sp, $sp, -4
-	sw $s0, 0($sp)
+	sw $s0, 0($sp) # salvo o indice da recursao na pilha
 	
 	addi $s0, $s0, -1
-	jal Fibonacci
+	jal Fibonacci # chamo a recursao para n - 1 /// F(n-1)
+	
+	# se todas as recursoes deste ponto já retornaram, então $s4 já deve conter o valor de F(n-1)	
+
 	addi $s0, $s0, -1
-	jal Fibonacci
+	jal Fibonacci # depois chamo a recursao para n - 2 /// F(n-2)
 	
-	lw $s0, 0($sp)
+	# se todas as recursoes deste ponto já retornaram, então $s4 já deve conter o valor de F(n-1) + F(n-2)
+	
+	lw $s0, 0($sp) # recebo o ultimo indice da recursao armazenado na pilha
+	
+	# caso ele ja tenha retornado todas as F(n-2) então o $s0 da pilha que ele está retirando foi o primeiro s0 da pilha
+	# então só resta receber o primeiro endereco de retorno da pilha, que é o que volta para a Main com o resultado
+	# armazenado em $s4.
+	
+	addi $sp, $sp, 4 
+	
+	# essa parte a cima eh necesaria para saber o valor retornado de F(n-1) + F(n-2)
+	
+	lw $ra, 0($sp) # retiro o ultimo endereco de retorno da pilha
 	addi $sp, $sp, 4
+	jr $ra # volto na recursao
 	
-	lw $ra, 0($sp)
-	addi $sp, $sp, 4
-	jr $ra
-	
-	return1:
-		addi $s4, $s4, 0
-		lw $ra, 0($sp) # recebe o valor armazenado do ultimo $ra da pilha
+	return0:
+		addi $s4, $s4, 0 # adiciona 0 ao resultado do numero de fibonacci
+		lw $ra, 0($sp) # recebe o ultimo endereco de retorno
 		addi $sp, $sp, 4
-		jr $ra
+		jr $ra # volta na recursao
 		
-	return2:
-		addi $s4, $s4, 1
-		lw $ra, 0($sp) # recebe o valor armazenado do ultimo $ra da pilha
+	return1:
+		addi $s4, $s4, 1 # adiciona 1 ao resultado do numero de fibonacci
+		lw $ra, 0($sp) # recebe o ultimo endereco para retornar
 		addi $sp, $sp, 4
-		jr $ra
+		jr $ra # volta na recursao
 
 EXIT:
